61fd5503fe229236d02c47e0b4e0bc94eb2cd951
Shivam Katare <katare27451@gmail.com>
2022-06-08 00:40:02 +0530

    Add new Scientific Function

--- package/index.js
+++ package/index.js
@@ -413,5 +413,37 @@ function getCurrency(currencyFrom, currencyTo, value) {
   return Math.round(result);
 }
 
+//Scientific Calculator
+
+/**
+ * @param {number} numValue The converted value
+ * @param {String} stringValue   Choose from sin, cos, or tan
+ */
+function getScientificValue(numValue, stringValue) {
+  let num = numValue;
+  let strValue = stringValue;
+  let output = 0;
+
+  switch (strValue) {
+    case "sin":
+      output = Math.sin(num);
+      break;
+    
+    case "cos":
+      output = Math.cos(num);
+      break;
+    
+    case "tan":
+      output = Math.tan(num);
+      break;
+    
+    default:
+      break;
+  }
+
+  //Returning the scientific value of the number
+  return output;
+}
+
 // Export functions
-module.exports = {doAdd,doAddN,doSub,doPositiveSub,doMul,doMulN,doDiv,doMod,calcAge,calcLoan,timeCalc,getCurrency, doReverse, toHex, toBin, toOct, toTri, toDec, doDecr, doInc};
+module.exports = {doAdd,doAddN,doSub,doPositiveSub,doMul,doMulN,doDiv,doMod,calcAge,calcLoan,timeCalc,getCurrency, doReverse, toHex, toBin, toOct, toTri, toDec, doDecr, doInc, getScientificValue};
