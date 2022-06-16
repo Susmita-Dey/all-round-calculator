6b9455e9ef476196edc6ea412b93c1cdf565b2df
Amaan Ahmad <amaanahmad582001@gmail.com>
2022-06-16 12:50:01 +0530

    Fixed exports: doFactorial, doMax, doMin

--- package/index.js
+++ package/index.js
@@ -1,6 +1,6 @@
 const { getCurrency } = require("./Functions/Currency");
 const doFactorial = require("./Functions/Factorial");
-const { doMax, doMin } = require("./Functions/Max.Min.Array")
+const { doMax, doMin } = require("./Functions/Max.Min.Array");
 
 /**
  * Returns the sum of two numbers
@@ -184,60 +184,60 @@ function calcLoan(amount, rate, month) {
  *
  * @param {number} seconds The number of seconds
  * @return {Data<number,number,number,number,number,number>} Object having year,month,day,hour,minute and second as properties
- * 
+ *
  */
 function timeCalc(seconds) {
-  let year = Math.floor(seconds / (3600*24*30*12));
-  let value= seconds%(3600*24*30*12)
-  
-  let month = Math.floor(value / (3600*24*30));
-  value%=(3600*24*30)
-  
+  let year = Math.floor(seconds / (3600 * 24 * 30 * 12));
+  let value = seconds % (3600 * 24 * 30 * 12);
+
+  let month = Math.floor(value / (3600 * 24 * 30));
+  value %= 3600 * 24 * 30;
+
   let day = Math.floor(value / 86400);
-  value%=86400
-  
+  value %= 86400;
+
   let hour = Math.floor(value / 3600);
-  value%=3600
-  
+  value %= 3600;
+
   let min = Math.floor(value / 60);
-  value%=60
-  
+  value %= 60;
+
   let sec = value;
 
-  const Obj={};
-//User can not modify the value of the properties
-  return Object.defineProperties(Obj,{
-       year:{
-          value:year,
-          writable:false,
-          enumerable:true
-       },
-       month:{
-          value:month,
-          writable:false,
-          enumerable:true
-      },
-      day:{
-          value:day,
-          writable:false,
-          enumerable:true
-      },
-      hour:{
-          value:hour,
-          writable:false,
-          enumerable:true
-      },
-      minute:{
-          value:min,
-          writable:false,
-          enumerable:true
-      },
-      second:{
-          value:sec,
-          writable:false,
-          enumerable:true
-      }
-   })
+  const Obj = {};
+  //User can not modify the value of the properties
+  return Object.defineProperties(Obj, {
+    year: {
+      value: year,
+      writable: false,
+      enumerable: true,
+    },
+    month: {
+      value: month,
+      writable: false,
+      enumerable: true,
+    },
+    day: {
+      value: day,
+      writable: false,
+      enumerable: true,
+    },
+    hour: {
+      value: hour,
+      writable: false,
+      enumerable: true,
+    },
+    minute: {
+      value: min,
+      writable: false,
+      enumerable: true,
+    },
+    second: {
+      value: sec,
+      writable: false,
+      enumerable: true,
+    },
+  });
 }
 //Scientific Calculator
 
@@ -254,15 +254,15 @@ function getScientificValue(numValue, stringValue) {
     case "sin":
       output = Math.sin(num);
       break;
-    
+
     case "cos":
       output = Math.cos(num);
       break;
-    
+
     case "tan":
       output = Math.tan(num);
       break;
-    
+
     default:
       break;
   }
@@ -272,4 +272,29 @@ function getScientificValue(numValue, stringValue) {
 }
 
 // Export functions
-module.exports = {doAdd,doAddN,doSub,doPositiveSub,doMul,doMulN,doDiv,doMod,calcAge,calcLoan,timeCalc,getCurrency, doReverse, toHex, toBin, toOct, toTri, toDec, doDecr, doInc, getScientificValue};
+module.exports = {
+  doAdd,
+  doAddN,
+  doSub,
+  doPositiveSub,
+  doMul,
+  doMulN,
+  doDiv,
+  doMod,
+  calcAge,
+  calcLoan,
+  timeCalc,
+  getCurrency,
+  doReverse,
+  toHex,
+  toBin,
+  toOct,
+  toTri,
+  toDec,
+  doDecr,
+  doInc,
+  getScientificValue,
+  doFactorial,
+  doMax,
+  doMin,
+};
